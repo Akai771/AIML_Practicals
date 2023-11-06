41173eab9ab2d4bd24699363e982c5687f900f13
Akai <rishabh.hs@somaiya.edu>
2023-11-06 19:53:46 +0530

    new MinMax code

--- minMax.py
+++ minMax.py
@@ -1,124 +1,111 @@
-"""-----------------------"""
-#This function is used to draw the board's current state every time the user turn arrives. 
-def ConstBoard(board):
-    print("Current State Of Board : \n\n");
-    for i in range (0,9):
-        if((i>0) and (i%3)==0):
-            print("\n");
-        if(board[i]==0):
-            print("- ",end=" ");
-        if (board[i]==1):
-            print("O ",end=" ");
-        if(board[i]==-1):    
-            print("X ",end=" ");
-    print("\n\n");
+player, opponent = 'x', 'o'
 
-#This function takes the user move as input and make the required changes on the board.
-def User1Turn(board):
-    pos=input("Enter X's position from [1...9]: ");
-    pos=int(pos);
-    if(board[pos-1]!=0):
-        print("Wrong Move!!!");
-        exit(0) ;
-    board[pos-1]=-1;
+# If the board has any empty ( _ ) value then it returns true
+def isMovesLeft(board) :
+	for i in range(3) :
+		for j in range(3) :
+			if (board[i][j] == '_') :
+				return True
+	return False
 
-def User2Turn(board):
-    pos=input("Enter O's position from [1...9]: ");
-    pos=int(pos);
-    if(board[pos-1]!=0):
-        print("Wrong Move!!!");
-        exit(0);
-    board[pos-1]=1;
+# This basically checks if the player or opponent has won or not
+def evaluate(b) :
+	
+	# Checking for Rows for X or O victory.
+	for row in range(3) :	
+		if (b[row][0] == b[row][1] and b[row][1] == b[row][2]) :		
+			if (b[row][0] == player) :
+				return 10
+			elif (b[row][0] == opponent) :
+				return -10
 
-#MinMax function.
-def minimax(board,player):
-    x=analyzeboard(board);
-    if(x!=0):
-        return (x*player);
-    pos=-1;
-    value=-2;
-    for i in range(0,9):
-        if(board[i]==0):
-            board[i]=player;
-            score=-minimax(board,(player*-1));
-            if(score>value):
-                value=score;
-                pos=i;
-            board[i]=0;
+	# Checking for Columns for X or O victory.
+	for col in range(3) :
+		if (b[0][col] == b[1][col] and b[1][col] == b[2][col]) :
+			if (b[0][col] == player) :
+				return 10
+			elif (b[0][col] == opponent) :
+				return -10
 
-    if(pos==-1):
-        return 0;
-    return value;
-    
-#This function makes the computer's move using minmax algorithm.
-def CompTurn(board):
-    pos=-1;
-    value=-2;
-    for i in range(0,9):
-        if(board[i]==0):
-            board[i]=1;
-            score=-minimax(board, -1);
-            board[i]=0;
-            if(score>value):
-                value=score;
-                pos=i;
- 
-    board[pos]=1;
+	# Checking for Diagonals for X or O victory.
+	if (b[0][0] == b[1][1] and b[1][1] == b[2][2]) :
+	
+		if (b[0][0] == player) :
+			return 10
+		elif (b[0][0] == opponent) :
+			return -10
+
+	if (b[0][2] == b[1][1] and b[1][1] == b[2][0]) :
+	
+		if (b[0][2] == player) :
+			return 10
+		elif (b[0][2] == opponent) :
+			return -10
+
+	# Else if none of them have won then return 0
+	return 0
 
 
-#This function is used to analyze a game.
-def analyzeboard(board):
-    cb=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+def minimax(board, depth, isMax) :
+	score = evaluate(board)
 
-    for i in range(0,8):
-        if(board[cb[i][0]] != 0 and
-           board[cb[i][0]] == board[cb[i][1]] and
-           board[cb[i][0]] == board[cb[i][2]]):
-            return board[cb[i][2]];
-    return 0;
+	if (score == 10) :
+		return score
 
-#Main Function.
-def main():
-    choice=input("Enter 1 for single player, 2 for multiplayer: ");
-    choice=int(choice);
-    #The broad is considered in the form of a single dimentional array.
-    #One player moves 1 and other move -1.
-    board=[0,0,0,0,0,0,0,0,0];
-    if(choice==1):
-        print("Computer : O Vs. You : X");
-        player= input("Enter to play 1(st) or 2(nd) :");
-        player = int(player);
-        for i in range (0,9):
-            if(analyzeboard(board)!=0):
-                break;
-            if((i+player)%2==0):
-                CompTurn(board);
-            else:
-                ConstBoard(board);
-                User1Turn(board);
-    else:
-        for i in range (0,9):
-            if(analyzeboard(board)!=0):
-                break;
-            if((i)%2==0):
-                ConstBoard(board);
-                User1Turn(board);
-            else:
-                ConstBoard(board);
-                User2Turn(board);
-         
+	if (score == -10) :
+		return score
+	
+	if (isMovesLeft(board) == False) :
+		return 0
 
-    x=analyzeboard(board);
-    if(x==0):
-         ConstBoard(board);
-         print("Draw!!!")
-    if(x==-1):
-         ConstBoard(board);
-         print("X Wins!!! Y Loose !!!")
-    if(x==1):
-         ConstBoard(board);
-         print("X Loose!!! O Wins !!!!")
-       
-#---------------#
-main()
-#---------------#
+	if (isMax) :	
+		best = -1000
+		for i in range(3) :		
+			for j in range(3) :
+
+				if (board[i][j]=='_') :
+					board[i][j] = player
+					best = max( best, minimax(board, depth + 1, not isMax) )
+					board[i][j] = '_'
+		return best
+
+	else :
+		best = 1000
+		for i in range(3) :		
+			for j in range(3) :
+				if (board[i][j] == '_') :
+					board[i][j] = opponent
+					best = min(best, minimax(board, depth + 1, not isMax))
+					board[i][j] = '_'
+		return best
+
+def findBestMove(board) :
+	bestVal = -1000
+	bestMove = (-1, -1)
+
+	for i in range(3) :	
+		for j in range(3) :
+			if (board[i][j] == '_') :
+
+				board[i][j] = player
+				moveVal = minimax(board, 0, False)
+				board[i][j] = '_'
+
+				if (moveVal > bestVal) :				
+					bestMove = (i, j)
+					bestVal = moveVal
+
+	print("The value of the best Move is :", bestVal)
+	print()
+	return bestMove
+
+board = [
+	[ 'x', 'o', 'x' ],
+	[ 'o', 'o', 'x' ],
+	[ '_', '_', '_' ]
+]
+
+bestMove = findBestMove(board)
+
+print("The Optimal Move is :")
+print("ROW:", bestMove[0], " COL:", bestMove[1])
