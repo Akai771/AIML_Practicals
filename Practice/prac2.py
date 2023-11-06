ba7b9a6d8f9694de3b6108011dbd8e68926c988a
Akai <rishabh.hs@somaiya.edu>
2023-11-06 19:19:27 +0530

    new dfs and bfs code

--- Practice/prac2.py
+++ Practice/prac2.py
@@ -1,35 +1,33 @@
-#for BFS
 from collections import defaultdict
+
 class Graph:
     def __init__(self):
         self.graph=defaultdict(list)
 
-    def addEdge (self,u,v):
+    def addEdge(self,u,v):
         self.graph[u].append(v)
 
-    def BFS (self,s):
-        visited= [False] * (len(self.graph))
+    def BFS(self,s):
+        visited =[False] * (len(self.graph))
         queue = []
         queue.append(s)
-        visited[s] = True
+        visited[s]=True
         
         while queue:
-            s = queue.pop(0)
+            s=queue.pop(0)
             print(s, end=" ")
             for i in self.graph[s]:
-                if visited[i] == False:
+                if visited[i]==False:
                     queue.append(i)
-                    visited[i] = True
+                    visited[i]=True
 
-#creating Graph
 g=Graph()
 g.addEdge(0,1)
 g.addEdge(0,2)
 g.addEdge(1,2)
 g.addEdge(2,0)
 g.addEdge(2,3)
-g.addEdge(3,3)
+g.addEdge(3,0)
 
 print("Following is BFS starting from vertex 2")
 g.BFS(2)
- 
